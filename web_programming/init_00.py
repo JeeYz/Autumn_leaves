5439042882aea505c8e771a1f35128825be6be08
JeeYz <Jkdspiegel@gmail.com>
2019-09-13 01:58:19 +0900

    hello world python web programming

---
+++ web_programming/init_00.py
@@ -0,0 +1,15 @@
+# @Author: J.Y.
+# @Date:   2019-09-13T01:57:48+09:00
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-09-13T01:58:02+09:00
+# @License: J.Y. JeeYz
+# @Copyright: J.Y. JeeYz
+
+
+print('hello, world')
+
+
+
+
+
+## endl
