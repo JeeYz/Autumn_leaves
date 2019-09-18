ed93f5ddc4cb9b1169987c637c2d0db851961bbc
JeeYz <Jkdspiegel@gmail.com>
2019-09-18 16:03:30 +0900

    hello, world~

--- web_programming/init_00.py
+++ web_programming/init_00.py
@@ -1,7 +1,7 @@
 # @Author: J.Y.
 # @Date:   2019-09-13T01:57:48+09:00
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-09-13T01:58:02+09:00
+# @Last modified time: 2019-09-18T16:03:14+09:00
 # @License: J.Y. JeeYz
 # @Copyright: J.Y. JeeYz
 
