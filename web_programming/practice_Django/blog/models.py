a57b9e42ccf9f169590c6c1eaaa512b6a24e9c0b
JeeYz <jkdspiegel@gmail.com>
2021-07-22 09:05:15 +0900

    첨부 파일 있을 경우, 포스트 상세 페이지에 다운로드 버튼 출력하기

--- web_programming/practice_Django/blog/models.py
+++ web_programming/practice_Django/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 # Create your models here.
+import os
 
 
 class Post(models.Model):
@@ -21,3 +22,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+
+
+
