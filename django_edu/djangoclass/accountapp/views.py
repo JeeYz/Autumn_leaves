d127d0f6d2d2f91f5bc9cb8b44778274893c15a5
JeeYz <jkdspiegel@gmail.com>
2022-07-13 11:20:57 +0900

    delete __pycache__ folders

--- django_edu/djangoclass/accountapp/views.py
+++ django_edu/djangoclass/accountapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'base.html')
 
 
 
--- django_edu/djangoclass/djangoclass/settings.py
+++ django_edu/djangoclass/djangoclass/settings.py
@@ -20,8 +20,8 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# # Set the project base directory
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,7 +68,9 @@ ROOT_URLCONF = 'djangoclass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
