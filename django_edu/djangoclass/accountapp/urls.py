cd678e6d5b27e361a102fb037e681437f12b87e1
JeeYz <jkdspiegel@gmail.com>
2022-07-19 12:57:50 +0900

    class backup, create account page

--- django_edu/djangoclass/accountapp/urls.py
+++ django_edu/djangoclass/accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 # from accountapp.views import hello_world
-from .views import hello_world
+from .views import AccountCreateView, hello_world
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
