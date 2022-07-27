94edf66ead2ed728752c6081a845f5f620aa5179
JeeYz <jkdspiegel@gmail.com>
2022-07-27 21:31:47 +0900

    generate profileapp

--- django_edu/djangoclass/accountapp/views.py
+++ django_edu/djangoclass/accountapp/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import method_decorator
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
--- django_edu/djangoclass/djangoclass/settings.py
+++ django_edu/djangoclass/djangoclass/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -152,5 +153,8 @@ LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
